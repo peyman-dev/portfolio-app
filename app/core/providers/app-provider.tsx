798912ef155877ec62c35c000d4fcef8e1886f5b
Peyman Ahmadi <peymangeek@gmail.com>
2025-09-05 17:54:47 +0330

    Font added to the @radix provider

--- app/core/providers/app-provider.tsx
+++ app/core/providers/app-provider.tsx
@@ -3,7 +3,7 @@ import { Theme } from "@radix-ui/themes";
 import React, { ReactNode } from "react";
 
 const ApplicationProvider = ({ children }: { children: ReactNode }) => {
-    return <Theme>
+    return <Theme className="font-Inter-Regular">
         {children}
     </Theme>
 };
