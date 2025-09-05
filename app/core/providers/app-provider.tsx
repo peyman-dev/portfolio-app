01cf3735ef669f1da2639c4b4dd096706b6b6cd7
Peyman Ahmadi <peymangeek@gmail.com>
2025-09-05 13:40:20 +0330

    Developed RadixUI provider and wrap the body into it

---
+++ app/core/providers/app-provider.tsx
@@ -0,0 +1,10 @@
+import { Theme } from "@radix-ui/themes";
+import React, { ReactNode } from "react";
+
+const ApplicationProvider = ({ children }: { children: ReactNode }) => {
+    return <Theme>
+        {children}
+    </Theme>
+};
+
+export default ApplicationProvider;
