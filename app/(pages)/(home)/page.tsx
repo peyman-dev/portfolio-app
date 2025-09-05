b339ebd0ebc1567920604585dbedc0c271b13b97
Peyman Ahmadi <peymangeek@gmail.com>
2025-09-05 17:13:02 +0330

    Initialized the Hero Section and imported to the home page

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,10 +1,12 @@
 import Header from '@/app/modules/common/header'
+import HeroSection from '@/app/modules/templates/home/hero-section'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <>
         <Header />
+        <HeroSection />
     </>
   )
 }
---
+++ app/modules/templates/home/hero-section/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeroSection = () => {
+  return <section id="hero-section" className="my-20">HeroSection</section>;
+};
+
+export default HeroSection;
