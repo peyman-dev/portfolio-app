2f7d4f6b9360119dc178ffe1dddc065c64c4e380
Peyman Ahmadi <peymangeek@gmail.com>
2025-09-11 20:39:18 +0330

    Created [useLocalTime] hook (for show my local time)

---
+++ app/core/hooks/use-local-time.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { displayTime } from "@/app/modules/common/header/settings";
+import React, { useEffect, useState } from "react";
+
+const useLocalTime = () => {
+  const [time, setTime] = useState("");
+
+  useEffect(() => {
+    const cleanup = displayTime(setTime);
+
+    return () => {
+      cleanup;
+    };
+  }, []);
+
+  return time
+};
+
+export default useLocalTime;
