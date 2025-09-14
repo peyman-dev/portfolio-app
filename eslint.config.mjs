31cd71903da20d03f21930d0c8dc6edc5d875c7a
Peyman Ahmadi <peymangeek@gmail.com>
2025-09-15 00:05:21 +0330

    ESLint config has been configed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,10 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
       "node_modules/**",
@@ -18,9 +12,6 @@ const eslintConfig = [
       "out/**",
       "build/**",
       "next-env.d.ts",
-      "@typescript-eslint/no-unused-vars",
-      "@typescript-eslint/no-explicit-any",
-      "react/no-unescaped-entities",
     ],
   },
 ];
