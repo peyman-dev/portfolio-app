ec34f949f8ee952dd6332bfb53300d547d23f8b1
Peyman Ahmadi <peymangeek@gmail.com>
2025-09-17 13:45:03 +0330

    .

--- app/_modules/common/articles/article/hero-details/index.tsx
+++ app/_modules/common/articles/article/hero-details/index.tsx
@@ -3,9 +3,14 @@ import Title from "./elements/title";
 
 const HeroDetails = () => {
   return (
-    <div className="flex-between">
+    <section id="article-hero" className="flex-between *:w-1/2 gap-[60px] container">
       <Title />
-    </div>
+      <div>
+        <p>
+          Peyman Ahmadi
+        </p>
+      </div>
+    </section>
   );
 };
 
