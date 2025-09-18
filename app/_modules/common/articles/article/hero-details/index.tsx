5e421fe2d58d59b4aa2df9696027f7896b903f15
Peyman Ahmadi <peymangeek@gmail.com>
2025-09-18 12:00:43 +0330

    Added Margin to the articles/[id]/[Hero Details] Component

--- app/_modules/common/articles/article/hero-details/index.tsx
+++ app/_modules/common/articles/article/hero-details/index.tsx
@@ -3,11 +3,15 @@ import Title from "./elements/title";
 
 const HeroDetails = () => {
   return (
-    <section id="article-hero" className="flex-between *:w-1/2 gap-[60px] container">
+    <section
+      id="article-hero"
+      className="flex-between *:w-1/2 gap-[60px] my-[100px] mb-[120px] container"
+    >
       <Title />
       <div>
-        <p>
-          Peyman Ahmadi
+        <p className="text-[40px] font-Inter-SemiBold!">Peyman Ahmadi</p>
+        <p className="text-[24px] text-[#808080] font-Inter-Medium!">
+          Thursday, 18 September 2025
         </p>
       </div>
     </section>
