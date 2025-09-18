46a28b31b3c865f29892fe8b3f81451dd7f3d4af
Peyman Ahmadi <peymangeek@gmail.com>
2025-09-18 11:58:39 +0330

    Created module named Disable Inspect (In live website)

---
+++ app/core/lib/disable-inspect.ts
@@ -0,0 +1,10 @@
+import toast from "react-hot-toast"
+
+const hostName = window.location.host
+
+document.body.onkeydown = (e: any) => {
+    if (e.key == "F12" && !hostName.includes("localhost")) {
+        toast.error("Yo, what you looking for? 😜")
+        return false
+    }
+}
