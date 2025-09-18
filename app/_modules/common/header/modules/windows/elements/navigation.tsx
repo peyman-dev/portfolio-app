24dae6f4531909404f478a0ec7913d2382256619
Peyman Ahmadi <peymangeek@gmail.com>
2025-09-18 12:02:45 +0330

    added [services] to the desktop navbar

--- app/_modules/common/header/modules/windows/elements/navigation.tsx
+++ app/_modules/common/header/modules/windows/elements/navigation.tsx
@@ -10,18 +10,36 @@ const Navigation = () => {
   return (
     <AsyncProvider>
       <div className="flex-ic **:cursor-pointer select-none gap-7">
-        <div onClick={() => startAsyncAction(async () => await redirect("/"))}>Home</div>
-        <div onClick={() => startAsyncAction(async () => await redirect("/playground"))}>
+        <div onClick={() => startAsyncAction(async () => await redirect("/"))}>
+          Home
+        </div>
+        <div
+          onClick={() =>
+            startAsyncAction(async () => await redirect("/articles"))
+          }
+        >
+          Articles
+        </div>
+        <div
+          onClick={() =>
+            startAsyncAction(async () => await redirect("/about-me"))
+          }
+        >
+          About me
+        </div>
+        <div
+          onClick={() =>
+            startAsyncAction(async () => await redirect("/playground"))
+          }
+        >
           Playground
         </div>
-        <div onClick={() => startAsyncAction(async () => await redirect("/services"))}>
+        <div
+          onClick={() =>
+            startAsyncAction(async () => await redirect("/services"))
+          }
+        >
           Services
-        </div>
-        <div onClick={() => startAsyncAction(async () => await redirect("/case-studies"))}>
-          Case studies
-        </div>
-        <div onClick={() => startAsyncAction(async () => await redirect("/about-me"))}>
-          About me
         </div>
       </div>
     </AsyncProvider>
