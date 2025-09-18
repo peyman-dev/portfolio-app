920dd161092516a20311003c5af097e159b7f1ba
Peyman Ahmadi <peymangeek@gmail.com>
2025-09-18 12:01:02 +0330

    Header gets hidden in dashboard route

--- app/_modules/common/header/index.tsx
+++ app/_modules/common/header/index.tsx
@@ -1,15 +1,22 @@
-"use client"
-import React from 'react'
-import WindowsNavbar from './modules/windows'
-import MobileNavbar from './modules/mobile'
+"use client";
+import React from "react";
+import WindowsNavbar from "./modules/windows";
+import MobileNavbar from "./modules/mobile";
+import { usePathname } from "next/navigation";
+import { isDashboardRoute } from "./settings";
 
 const Header = () => {
+  const pathname = usePathname();
+
+  if (isDashboardRoute(pathname)) {
+    return null;
+  }
   return (
     <header>
-        <WindowsNavbar />
-        <MobileNavbar />
+      <WindowsNavbar />
+      <MobileNavbar />
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
