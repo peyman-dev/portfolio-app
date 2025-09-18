bc4efd4b86e58b5d01ea9fb80fb39485f3b3e1c9
Peyman Ahmadi <peymangeek@gmail.com>
2025-09-18 14:28:44 +0330

    Article hero section responsived

--- app/_modules/common/articles/article/hero-details/index.tsx
+++ app/_modules/common/articles/article/hero-details/index.tsx
@@ -5,12 +5,12 @@ const HeroDetails = () => {
   return (
     <section
       id="article-hero"
-      className="flex-between *:w-1/2 gap-[60px] my-[100px] mb-[120px] container"
+      className="flex-between lg:flex-row flex-col *:w-full lg:*:w-1/2 gap-[60px] my-[100px] mb-[120px] container"
     >
       <Title />
       <div>
-        <p className="text-[40px] font-Inter-SemiBold!">Peyman Ahmadi</p>
-        <p className="text-[24px] text-[#808080] font-Inter-Medium!">
+        <p className="lg:text-[40px] text-xl font-Inter-SemiBold!">Peyman Ahmadi</p>
+        <p className="lg:text-[24px] text-sm text-[#808080] font-Inter-Medium!">
           Thursday, 18 September 2025
         </p>
       </div>
