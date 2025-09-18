d344f019bdb32a22acc7567007946a5a9219d5fb
Peyman Ahmadi <peymangeek@gmail.com>
2025-09-18 11:59:52 +0330

    Added Toaster & Disable Inspect To the [App Provider]

--- app/core/providers/app-provider.tsx
+++ app/core/providers/app-provider.tsx
@@ -1,9 +1,12 @@
 "use client"
 import { Theme } from "@radix-ui/themes";
 import React, { ReactNode } from "react";
+import '../lib/disable-inspect'
+import { Toaster } from "react-hot-toast";
 
 const ApplicationProvider = ({ children }: { children: ReactNode }) => {
     return <Theme className="font-Inter-Regular">
+        <Toaster />
         {children}
     </Theme>
 };
