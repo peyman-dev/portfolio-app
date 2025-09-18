4f687cad9336efdd6cb9673da14ba79ab0022f9d
Peyman Ahmadi <peymangeek@gmail.com>
2025-09-18 14:17:19 +0330

    Initialized the /dashboard/[settings] file

---
+++ app/dashboard/settings/styles.ts
@@ -0,0 +1,6 @@
+import clsx from "clsx";
+
+export const classNames = {
+    layout: clsx("min-h-screen flex bg-[#D8DCE5]"),
+    sidebar: clsx("bg-[#041228] h-screen w-[96px] flex-col-between text-white")
+}
